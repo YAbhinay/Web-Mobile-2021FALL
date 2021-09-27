bbade45fabe1d8ad458a99f12fbbed29d8ea1cf6
YAbhinay <89653700+YAbhinay@users.noreply.github.com>
2021-09-27 05:05:45 -0500

    ss

--- Web ICP/ICP 5/todo/src/app/app.module.ts
+++ Web ICP/ICP 5/todo/src/app/app.module.ts
@@ -1,9 +1,14 @@
+// @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';
+// @ts-ignore
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// @ts-ignore
 import {FormsModule} from '@angular/forms';
+
+// @ts-ignore
 
 @NgModule({
   declarations: [
